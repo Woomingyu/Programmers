901cc625fc3d8f8dfbfeb735883abeed6912a5e4
Woomingyu <119576417+Woomingyu@users.noreply.github.com>
2023-08-24 10:58:58 +0900

    [level 1] Title: 평균 구하기, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i = 0; i < arr.Length; i++)
+        {
+            answer += arr[i];
+        }
+                 
+        return answer /= arr.Length;
+    }
+}
